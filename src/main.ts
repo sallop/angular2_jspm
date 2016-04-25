c44dc686be515333adf9897b4d3b43c0eb8b103d
sallop <shiogai1987@yahoo.co.jp>
2016-04-25 12:44:36 +0900

    it works

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// import deps
+//import 'zone';
+import 'zone.js/dist/zone';
+import 'reflect-metadata';
+import 'es6-shim';
+
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
