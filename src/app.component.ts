271cfe0ae072069b1c9f9a093a5ae7c6404f06c7
sallop <shiogai1987@yahoo.co.jp>
2016-04-25 15:51:04 +0900

    property binding

--- src/app.component.ts
+++ src/app.component.ts
@@ -11,7 +11,9 @@ export class Hero {
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="#hero of heroes" (click)="onSelect(hero)">
+    <li *ngFor="#hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
       <!-- each hero goes here -->
       <span class="badge">{{hero.id}}</span>{{hero.name}}
     </li>
